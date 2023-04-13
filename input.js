9ba2e4b1124eba903aeee98c9fb1494951948780
Arian Ahmadi <arianahmadi1@gmail.com>
2023-04-13 05:45:18 -0400

    snake game 0.2

--- input.js
+++ input.js
@@ -1,13 +1,24 @@
-const setupInput = function() {
+let connection;
+
+const setupInput = function(conn) {
+  connection = conn;
+
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
-  //have to defiene handleUserInput function first before calling stdin.on to listen.
+  
   const handleUserInput = function(key) {
-    if (key === "\u0003") {
-      // check for CTRL+C input
+    if (key === '\u0003') { // check for CTRL+C input
       process.exit();
+    } else if (key === 'w') {
+      connection.write("Move: up");
+    } else if (key === 'a') {
+      connection.write("Move: left");
+    } else if (key === 's') {
+      connection.write("Move: down");
+    } else if (key === 'd') {
+      connection.write("Move: right");
     }
   };
 
