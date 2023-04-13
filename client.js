25e96e019935f71095a99dd6fcff9bdad2111b90
Arian Ahmadi <arianahmadi1@gmail.com>
2023-04-12 22:39:49 -0400

    client.js

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 const net = require("net");
 
 // establishes a connection with the game server
-const connect = function() {
+const connect = function () {
   const conn = net.createConnection({
     host: "165.227.47.243",
     port: 50541,
@@ -15,7 +15,15 @@ const connect = function() {
     console.log(data);
   });
 
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+  }); //this is an event
+
+  conn.write("Name: ARY"); //what shows on the server
+
+  // conn.write("Move: up");
+
   return conn;
 };
 
-module.exports = {connect};
+module.exports = { connect };
